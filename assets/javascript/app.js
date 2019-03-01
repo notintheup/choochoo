1654cd127074632d012fd40159b97b1b6d5cb034
notintheup <brianjandron@gmail.com>
2019-03-01 18:57:59 -0500

    divs and formations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,42 @@ var database = firebase.database();
     var trainName = "";
     var trainDestination = "";
     var trainTime = 0;
-    var trainFrequency = 0
+    var trainFrequency = 0;
 
 
+    // Capture Button Click
+    $("#subClick").on("click", function(event) {
+      // Don't refresh the page!
+      event.preventDefault();
+
+      //Store and retrieve information to database
+      trainName = $("#inputTrain").val().trim();
+      trainDestination = $("#inputDestination").val().trim();
+      trainTime = $("#inputTime").val().trim();
+      trainFrequency = $("#inputFrequency").val().trim();
+
+      database.ref().set({
+        trainName: trainName,
+        trainDestination: trainDestination,
+        trainTime: trainTime,
+        trainFrequency: trainFrequency
+      });
+
+    });
+
+    database.ref().on("value", function(snapshot) {
+
+      // Log everything that's coming out of snapshot
+      console.log(snapshot.val().trainName);
+      console.log(snapshot.val().trainDestination);
+      console.log(snapshot.val().trainTime);
+      console.log(snapshot.val().trainFrequency);
+    
+          // Change the HTML to reflect
+          $("#inputTrain").text(snapshot.val().trainName);
+          $("#inputDestination").text(snapshot.val().trainDestination);
+          $("#inputTime").text(snapshot.val().trainTime);
+          $("#inputFrequency").text(snapshot.val().trainFrequency);
+    
+    })
 
